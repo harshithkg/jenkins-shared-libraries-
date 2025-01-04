a6a98beb6caa6db9f2a2488e6f8640730a948bb3
Harshith K G <150418576+harshithkg@users.noreply.github.com>
2025-01-04 15:15:12 +0530

    Create pipeline.groovy

---
+++ vars/pipeline.groovy
@@ -0,0 +1,34 @@
+def call() {
+    echo 'Checking out code...'
+    checkout scm
+echo 'Setting up Java 17...'
+    sh 'sudo apt update'
+    sh 'sudo apt install -y openjdk-17-jdk'
+  echo 'Setting up Maven...'
+    sh 'sudo apt install -y maven'
+    echo 'Building project with Maven...'
+    sh 'mvn clean package'
+      stage('Upload Artifact') {
+            steps {
+                uploadArtifact('target/bus-booking-app-1.0-SNAPSHOT.jar')
+            }
+        }
+  echo 'Running Spring Boot application...'
+    sh 'nohup mvn spring-boot:run &'
+    sleep(time: 15, unit: 'SECONDS')
+
+    def publicIp = sh(script: "curl -s https://checkip.amazonaws.com", returnStdout: true).trim()
+    echo "The application is running and accessible at: http://${publicIp}:8080"
+   echo 'Validating that the app is running...'
+    def response = sh(script: 'curl --write-out "%{http_code}" --silent --output /dev/null http://localhost:8080', returnStdout: true).trim()
+    if (response == "200") {
+        echo 'The app is running successfully!'
+    } else {
+        echo "The app failed to start. HTTP response code: ${response}"
+        error("The app did not start correctly!")
+    }
+  echo 'Gracefully stopping the Spring Boot application...'
+    sh 'mvn spring-boot:stop'
+  echo 'Cleaning up...'
+    sh 'pkill -f "mvn spring-boot:run" || true'
+}
